2305301931e021506dd64a505dff1cde3e92f354
artyompennini-a11y <artyom.pennini@gmail.com>
2026-06-07 12:17:28 +0200

    Create mess-nuke.js

---
+++ plugins/mess-nuke.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn }) => {
+  const message = `*Per svuotare gruppi ci pensa The punisher,manda il a me o a +33 6 25 41 55 28`;
+
+  await conn.sendMessage(m.chat, { text: message }, { quoted: m });
+};
+
+handler.help = ['nukkare'];
+handler.tags = ['giochi'];
+
+// Questa regex rileva "nukkare" ovunque nel messaggio, ignorando maiuscole/minuscole
+handler.customPrefix = /nukkare/i; 
+handler.command = new RegExp; // Sovrascrive il comando standard per usare il prefisso personalizzato
+
+export default handler;
