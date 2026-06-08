5654ea736362b16d8bfb4e259bb55ee7f35af77b
artyompennini-a11y <artyom.pennini@gmail.com>
2026-06-08 23:04:57 +0200

    Change User-Agent header in axios request

--- plugins/giochi-verofalso.js
+++ plugins/giochi-verofalso.js
@@ -12,7 +12,7 @@ const handler = async (m, { conn }) => {
     const res = await axios.get(randomVideo, {
       responseType: 'arraybuffer',
       headers: {
-        'User-Agent': '𝕰𝕷𝕴𝖃𝕴𝕽𝕭𝕺𝕿',
+        'User-Agent': '𝚃𝙷𝙴 𝙿𝚄𝙽𝙸𝚂𝙷𝙴𝚁-𝙱𝙾𝚃',
         'Accept': 'video/mp4'
       }
     })
